ec966d6340708cc9d6a7639fe08aaa301d74077a
xinpingdeng <xinping.deng@gmail.com>
2021-11-26 11:39:03 +1100

    old pipeline CRACO-42 runs fine with small dataset

--- src/craco/test_pipeline.py
+++ src/craco/test_pipeline.py
@@ -64,6 +64,9 @@ NSMP_2DFFT  = (NPIX*NPIX)
 MAX_NSMP_UV = 4800 # This should match the number in pipeline krnl.hpp file
 MAX_NPARALLEL_UV = (MAX_NSMP_UV//2)
 
+#NEW_GRID = True
+NEW_GRID = False
+
 class DdgridCu(Kernel):
     def __init__(self, device, xbin):
         super().__init__(device, xbin, 'krnl_ddgrid_reader_4cu:krnl_ddgrid_reader_4cu_1')
@@ -145,14 +148,16 @@ def get_grid_lut_from_plan(plan):
 class Pipeline:
     def __init__(self, device, xbin, plan_fname):
         self.plan = load_plan(plan_fname)
-        # If we are on new version of pipeline
-        self.nparallel_uvin, self.nparallel_uvout, self.h_nparallel_uvout, lut = get_grid_lut_from_plan(self.plan)
 
-        ## if we are on old version of pipeline, which grid does not have accumulation function
-        #lutbin = 'none_duplicate_long.uvgrid.txt.bin'
-        #print(f'Using lut binary file {lutbin}')
-        #lut = np.fromfile(lutbin, dtype=np.uint32)
-        #print(f'LUT size is {len(lut)}')
+        if NEW_GRID:
+            # If we are on new version of pipeline
+            self.nparallel_uvin, self.nparallel_uvout, self.h_nparallel_uvout, lut = get_grid_lut_from_plan(self.plan)
+        else:
+            # if we are on old version of pipeline, which grid does not have accumulation function
+            lutbin = 'none_duplicate_long.uvgrid.txt.bin'
+            print(f'Using lut binary file {lutbin}')
+            lut = np.fromfile(lutbin, dtype=np.uint32)
+            print(f'LUT size is {len(lut)}')
         
         self.grid_reader = DdgridCu(device, xbin)
         self.grids = [GridCu(device, xbin, i) for i in range(4)]
@@ -164,16 +169,17 @@ class Pipeline:
         print(f'nuv {self.plan.fdmt_plan.nuvtotal}')
         
         print('Allocating grid LUTs')
-        # For grid with new version pipeline
-        self.grid_luts = [Buffer(lut.shape, np.uint32, device, g.krnl.group_id(5)).clear() for g in self.grids]
+        if NEW_GRID:
+            # For grid with new version pipeline
+            self.grid_luts = [Buffer(lut.shape, np.uint32, device, g.krnl.group_id(5)).clear() for g in self.grids]
+        else:
+            # For grid with old version pipeline
+            self.grid_luts = [Buffer(lut.shape, np.uint32, device, g.krnl.group_id(3)).clear() for g in self.grids]
 
-        # For grid with old version pipeline
-        #self.grid_luts = [Buffer(lut.shape, np.uint32, device, g.krnl.group_id(3)).clear() for g in self.grids]
         for l in self.grid_luts:
             l.nparr[:] = lut
             l.copy_to_device()
-        #exit()
-        
+                
         # FDMT: (pin, pout, histin, histout, pconfig, out_tbkl)
         print('Allocating FDMT Input')
 
@@ -234,14 +240,17 @@ def run(p, blk, values):
 
     print(f'ndm={ndm} nchunk_time={nchunk_time} tblk={tblk} nuv={nuv} nparallel_uv={nparallel_uv} nurest={nurest} load_luts={load_luts} nplane={nplane} shift1={shift1} shift2={shift2} fft_cfg={fft_cfg}')
 
-    values.run_pipeline = False #True
-    values.run_fdmt = True
+    #values.run_pipeline = False #True
+    values.run_pipeline = True
+    values.run_fdmt     = True
 
     assert ndm < 1024 # It hangs for 1024 - not sure why.
 
     starts = []
 
-    assert nparallel_uv == self.nparallel_uvin # the number from pipeline plan should be the same as we calculated based on indexs from pipeline plan
+    if NEW_GRID:
+        assert nparallel_uv == self.nparallel_uvin # the number from pipeline plan should be the same as we calculated based on indexs from pipeline plan
+        
     if values.run_pipeline:
         #assert nuv == 3440 # NUV and the LUT need to agree - if not you get in trouble
         for cu in self.ffts:
@@ -251,17 +260,16 @@ def run(p, blk, values):
         starts.append(self.grid_reader(self.mainbuf, ndm, tblk, nchunk_time, nurest, self.ddreader_lut, load_luts))
 
         for cu, grid_lut in zip(self.grids, self.grid_luts):
-            # For grid with new pipeline
-            starts.append(cu(ndm, nchunk_time, self.nparallel_uvin, self.nparallel_uvout, self.h_nparallel_uvout, grid_lut, load_luts))
-
-            ## For grid with old pipeline
-            #starts.append(cu(ndm, nchunk_time, nparallel_uv, grid_lut, load_luts))
+            if NEW_GRID:
+                # For grid with new pipeline
+                starts.append(cu(ndm, nchunk_time, self.nparallel_uvin, self.nparallel_uvout, self.h_nparallel_uvout, grid_lut, load_luts))
+            else:
+                # For grid with old pipeline
+                starts.append(cu(ndm, nchunk_time, nparallel_uv, grid_lut, load_luts))
 
     if values.run_fdmt:
         starts.append(self.fdmtcu(self.inbuf, self.mainbuf, self.fdmt_hist_buf, self.fdmt_hist_buf, self.fdmt_config_buf, nurest, tblk))
 
-
-    
     return starts
 
 
@@ -283,8 +291,11 @@ def _main():
     parser.add_argument('-p', '--plan', default='pipeline_short.pickle', type=str, action='store', help='plan file which has pipeline configurations')
 
     parser.set_defaults(verbose=False)
-    parser.set_defaults(xclbin="binary_container_1.xclbin.CRACO-46")
-    
+    if NEW_GRID:
+        parser.set_defaults(xclbin="binary_container_1.xclbin.CRACO-46")
+    else:
+        parser.set_defaults(xclbin="binary_container_1.xclbin.CRACO-42")
+        
     values = parser.parse_args()
     if values.verbose:
         logging.basicConfig(level=logging.DEBUG)
