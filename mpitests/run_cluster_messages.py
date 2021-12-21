0de38484ba1b55b22c2c965b83ebe42faa974fb4
Keith Bannister <keith.bannister@csiro.au>
2021-12-21 17:05:39 +1100

    Send receiver info on run-cluster messages

--- mpitests/run_cluster_messages.py
+++ mpitests/run_cluster_messages.py
@@ -14,6 +14,7 @@ import logging
 from mpi4py import MPI
 import mpi4py
 import time
+import random
 
 log = logging.getLogger(__name__)
 
@@ -46,8 +47,18 @@ def be_receiver(values):
     transmitters = world.Split(0, rank)
     transmit_ids = np.arange(values.nlink)
     receive_ids = transmit_ids % values.nrx
-    num_transmitters_sending_to_me = sum(receive_ids == rank)
-    log.info(f'Rank {rank} expects data from {num_transmitters_sending_to_me} transmitters')
+    my_transmitters = np.where(receive_ids == rank)[0]
+    num_transmitters_sending_to_me = len(my_transmitters)
+    log.info(f'Rank {rank} expects data from {num_transmitters_sending_to_me} transmitters:{my_transmitters}')
+    receiver_info = {'rank':rank, 'psn':random.randint(0, 16384), 'qpn': random.randint(0, 16384),
+                     'gid': np.random.bytes(16), 'lid':np.random.randint(0,16384)}
+    
+
+    # Send info to my transmitters
+    for tx in my_transmitters:
+        destrank = tx + values.nrx
+        log.info(f'Sending {receiver_info} to rank {destrank}')
+        world.send(receiver_info, dest=int(destrank), tag=1)
 
     msg = np.zeros(values.msg_size)
     status = MPI.Status()
@@ -55,8 +66,9 @@ def be_receiver(values):
     for imsg in range(values.nmsg):
         # TODO: extra loop over the number of transmitters I'm exxpecting
         for imsg2 in range(num_transmitters_sending_to_me):
-            world.Recv(msg, MPI.ANY_SOURCE, MPI.ANY_TAG, status)
-            log.debug(f'Receviver {rank} Got data from transmitter={status.Get_source()} tag={status.Get_tag()} mean={msg.mean()}')
+            if values.method == 'mpi':
+                world.Recv(msg, MPI.ANY_SOURCE, MPI.ANY_TAG, status)
+                log.debug(f'Receviver {rank} Got data from transmitter={status.Get_source()} tag={status.Get_tag()} mean={msg.mean()}')
 
     end = time.time()
     interval = end - start
@@ -78,10 +90,17 @@ def be_transmitter(values):
 
     # the reciver rank we'll send our data  to
     target_receiver_rank = transmit_rank % values.nrx
+
+    # Wait for info from my receiver
+    status = MPI.Status()
+    info = world.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+    log.info(f'Got {info} from rank={status.Get_source()} tag={status.Get_tag()}')
+    
     msg = np.zeros(values.msg_size)
     for imsg in range(values.nmsg):
-        log.debug(f'Sending msg {imsg} to {target_receiver_rank} from {transmit_rank}')
-        world.Send(msg+imsg, dest=target_receiver_rank, tag=transmit_rank)
+        log.debug(f'Sending msg {imsg} from {transmit_rank} to {target_receiver_rank}')
+        if values.method == 'mpi':
+            world.Send(msg+imsg, dest=target_receiver_rank, tag=transmit_rank)
         
 
 def _main():
@@ -92,6 +111,7 @@ def _main():
     parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
     parser.add_argument('--nmsg', type=int, default=1000)
     parser.add_argument('--msg-size', type=int, default=4096)
+    parser.add_argument('--method', default='mpi', help='mpi or rdma')
     parser.set_defaults(verbose=False)
     values = parser.parse_args()
     if values.verbose:
