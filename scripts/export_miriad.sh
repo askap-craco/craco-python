9dc4926f5367d14228b740535bfc343e470607b6
Keith Bannister <keith.bannister@csiro.au>
2022-09-29 16:42:07 +1000

    Add export_miriad script

---
+++ scripts/export_miriad.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mir=$1
+#mfcal vis=$mir
+for opt in gain bandpass ; do
+    for yaxis in real imag amp phase; do
+        gpplt vis=$mir log=${mir}.${opt}.${yaxis} yaxis=${yaxis} options=${opt} nxy=6,6 device=${opt}.${yaxis}.png/png
+    done
+done
