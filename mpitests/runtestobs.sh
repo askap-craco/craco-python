38a285c94f31c12b48c0940eb90762d86b1371e0
Keith Bannister <keith.banniser@csiro.au>
2023-03-27 12:28:57 +0800

    Fix runtestobs

--- mpitests/runtestobs.sh
+++ mpitests/runtestobs.sh
@@ -17,7 +17,7 @@ for spi in 16 32 64 ; do
     for pol in ps dp ; do
 	for beam in 0 -1; do
 	    for fpga in 1-6 ; do
-		for cards in $cards ; do
+		for card in $cards ; do
 		    for ts in  off on; do 
 			if [[ $pol == "ps" ]] ; then
 			    polcmd="--pol-sum"
@@ -35,10 +35,10 @@ for spi in 16 32 64 ; do
 			    continue 
 			fi
 			      
-			dout="$CARDCAP_DIR/cap_spi${spi}_bm${beam}_a${cards}_k${fpga}_$pol_ts$ts"
-			cmd="$ccap --tscrunch $tscrunch --samples-per-integration $spi $polcmd --beam $beam -a $cards -k $fpga --num-msgs $nmsg -f $dout/$target.fits"
+			dout="$CARDCAP_DIR/cap_spi${spi}_bm${beam}_a${cards}_k${fpga}_${pol}_ts$ts"
+			cmd="$ccap --tscrunch $tscrunch --samples-per-integration $spi $polcmd --beam $beam -a $card -k $fpga --num-msgs $nmsg -f $dout/$target.fits"
 			echo $cmd
-			$cmd
+			#$cmd
 		    done
 		done
 	    done
