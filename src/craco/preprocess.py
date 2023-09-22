663401ae707cc709713f351761f5d5a43c4a9a15
Vivek Gupta <gup037@seren-01.atnf.CSIRO.AU>
2023-09-22 18:57:00 +0800

    Fixed np.mean() datatype change behaviour with masked_array input issue in normalise function of preprocess.py

--- src/craco/preprocess.py
+++ src/craco/preprocess.py
@@ -57,10 +57,12 @@ def normalise(block, target_input_rms = 1):
             #print(f"====>> The shape of received block[ibl] for ibl{ibl} is {block[ibl].shape}")
             existing_rms = bldata.std(axis=-1) / np.sqrt(2)
             new_block[ibl] = (bldata - np.mean(bldata, axis=-1, keepdims=True)) * (target_input_rms / existing_rms)[..., None]
+            new_block[ibl] = new_block[ibl].astype(bldata.dtype)
             #print(f"====>> The shape of normalised block[ibl] for ibl{ibl} is {block[ibl].shape}")
     elif type(block) == np.ndarray or type(block) == np.ma.core.MaskedArray:
         existing_rms = block.std(axis=-1) / np.sqrt(2)   
         new_block = (block - np.mean(block, axis=-1, keepdims = True)) * (target_input_rms / existing_rms)[..., None]
+        new_block = new_block.astype(block.dtype)
 
     else:
         raise Exception("Unknown type of block provided - expecting dict, np.ndarray, or np.ma.core.MaskedArray")
