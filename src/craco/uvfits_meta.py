89190d6a165ec3273b5427a89b5cb2ec57448d03
Keith Bannister <keith.bannister@csiro.au>
2023-11-21 15:23:23 +1100

    Make uvfits meta not pass on other values when defaulting to uvfits

--- src/craco/uvfits_meta.py
+++ src/craco/uvfits_meta.py
@@ -85,6 +85,7 @@ def open(*args, **kwargs):
     logging.info('Opening file %s', args[0])
     mfile = kwargs.get('metadata_file', None)
     if mfile is None:
+        del kwargs['metadata_file']
         x = uvfits.open(*args, **kwargs)
     else:
         x = UvfitsMeta(fits.open(*args, **kwargs), **kwargs)
