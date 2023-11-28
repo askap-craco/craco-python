01db94c42db15cd098d510554e696c23c0bf4a44
vivgastro <vivg269@gmail.com>
2023-11-28 12:24:37 +0800

    Removed import of candpipe from init.py

--- src/craco/__init__.py
+++ src/craco/__init__.py
@@ -2,7 +2,9 @@ __version__ = "0.0.1"
 __author__ = "Keith Bannister <keith.bannister@csiro.au>"
 # If we import this then we get exceptions if we dont have the sklearn dependency
 # if we dont import it - Im not sure what happens
-try:
-    from . import candpipe
-except ModuleNotFoundError:
-    pass
+
+
+#try:
+#    from . import candpipe
+#except ModuleNotFoundError:
+#    pass
