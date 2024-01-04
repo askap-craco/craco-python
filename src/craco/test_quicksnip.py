9e01e6da118fc0692d1e65965cfa3ba0beb10439
Keith Bannister <keith.bannister@csiro.au>
2024-01-04 19:28:54 +1100

    Add test for quicksnip with metadata

--- src/craco/test_quicksnip.py
+++ src/craco/test_quicksnip.py
@@ -15,6 +15,8 @@ import pytest
 from astropy.io import fits
 from craco.quicksnip import snip
 from IPython import embed
+from numpy.testing import assert_allclose
+
 
 log = logging.getLogger(__name__)
 
@@ -37,7 +39,28 @@ def test_snip_works():
     assert np.all(ind.data[:n]['DATA'] == outd.data[:n]['DATA'])
     for intab, outtab in zip(inhdu[1:], outhdu[1:]):
         assert np.all(intab.data == outtab.data)
+
+def test_snip_works_with_metadata():
+    infile = 'testdata/SB053972/b00.uvfits'
+    metafile = 'testdata/SB053972/SB53972.json.gz'
+    inhdu = fits.open(infile)
+    outfile = 'snipped.uvfits'
+    snip(infile, outfile, 0, 100,metafile)
+    outhdu = fits.open(outfile)
+    assert len(inhdu) == len(outhdu)
+    ind = inhdu[0]
+    outd = outhdu[0]
+    n = len(outd.data)
+    for p in ind.parnames:
+        if p != 'DATE':
+            assert_allclose(ind.data[p][:n], outd.data[p][:n],  rtol=1e-6) #,err_msg=f'{p} not equal')
+
+    # only check data - don't check weights
+    assert np.all(ind.data[:n]['DATA'][...,:2] == outd.data[:n]['DATA'][...,:2])
+    for intab, outtab in zip(inhdu[1:], outhdu[1:]):
+        assert np.all(intab.data == outtab.data)
     
+
 
     
 def _main():
