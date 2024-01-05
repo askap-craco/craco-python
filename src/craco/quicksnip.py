0065bb35d3f39753de0a34e731ad532681e097cd
Keith Bannister <keith.bannister@csiro.au>
2024-01-05 13:06:47 +0800

    More logging for quicksinip

--- src/craco/quicksnip.py
+++ src/craco/quicksnip.py
@@ -28,9 +28,9 @@ def snip(infile, outfile, startidx, nblk, metadata_file=None):
         gcount = 0
         tbytes = len(hdrb)
         assert len(hdrb) % 2880 == 0
+        log.debug('Opened %s to %s start=%s nblk=%s hdr=%s vissize=%s nsamp=%s', infile, outfile, startidx, nblk, len(hdrb), inf.vis.size, inf.nsamps)
         for iblk, dout in enumerate(inf.fast_raw_blocks(istart=startidx, nsamp=nblk, nt=1, raw_date=True)):
             dout.tofile(fout)
-            from IPython import embed
             gcount += dout.size
             tbytes += dout.size*dout.dtype.itemsize
 
@@ -39,6 +39,7 @@ def snip(infile, outfile, startidx, nblk, metadata_file=None):
         fout.seek(0,0)
         fout.write(bytes(hdr.tostring(), 'utf-8'))
         fout.flush()
+        log.debug('Finished writing %s. Gcount=%s', outfile, gcount)
         
     fixuvfits.fix_length(outfile)
     inhdu = fits.open(infile)
