03acfbf313a2b5e819288c3e2e689c4005e9b8a3
Vivek Gupta <vivg269@gmail.com>
2024-01-30 13:12:29 +1100

    First fully working version of uvfits_writer and extract.py

--- src/craco/uvfits_extract.py
+++ src/craco/uvfits_extract.py
@@ -1,4 +1,5 @@
-from craco import uvfits_meta
+from craco import uvfits_meta, preprocess
+from craft import craco_plan
 from craco.uvfits_writer import UvfitsWriter, copy_visparams_to_visrow
 import numpy as np
 import argparse
@@ -15,13 +16,39 @@ def main(args):
     of = UvfitsWriter(outname, infile=args.uvpath)
     of.copy_header()
 
+    if args.dedisp_pccc:
+        ddp = preprocess.Dedisp(f.channel_frequencies, 
+                                f.tsamp.value,
+                                dm_pccc = args.dedisp_pccc)
+
+
+    if args.calib:
+        print(f"Starting calibration using {args.calib}")
+        plan_args = " "
+        if args.flag_ants:
+            plan_args += f"--flag-ants {args.flag_ants}"
+
+        plan = craco_plan.PipelinePlan(f, plan_args)
+        calibrator = preprocess.Calibrate(plan = plan,
+                                block_dtype=np.ndarray,
+                                miriad_gains_file=args.calib,
+                                baseline_order=f.internal_baseline_order)
+    
     for iblk, visout in enumerate(f.fast_raw_blocks(nsamp = nsamps_to_read, nt = 1, raw_date=True)):
         data_block = f.convert_visrows_into_block(visout)
-        modified_data = data_block.copy()
-        modified_visdata = f.convert_block_into_visrows(modified_data)
-        print(modified_data.shape,modified_visdata.shape,  modified_visdata["DATA"].squeeze().flatten().shape)
-        print(data_block.shape, data_block.squeeze().flatten().shape)
-        assert np.all(modified_visdata["DATA"].squeeze().flatten() == data_block.squeeze().flatten())
+        print(f"Shape of data_block is {data_block.shape}")
+        #modified_data = data_block.copy()
+        if args.calib:
+            data_block = calibrator.apply_calibration(data_block[:, 0, 0, 0, :, 0, :])
+        if args.sky_subtract:
+            data_block = preprocess.normalise(data_block)
+        if args.dedisp_pccc:
+            data_block = preprocess.fill_masked_values(data_block, fill_value = 0)
+            data_block = ddp.dedisperse(iblk, inblock=data_block)
+
+        #print(f"Shape of data_block after processing is {data_block.shape}, type is {type(data_block)}, mask is {data_block.mask}")
+
+        modified_visdata = f.convert_block_into_visrows(data_block)
         UU = visout['UU'].flatten()
         VV = visout['VV'].flatten()
         WW = visout['WW'].flatten()
@@ -48,6 +75,10 @@ if __name__ == '__main__':
     a.add_argument("-apply-metadata-masks", type=bool, help="Apply metadata masks? (def=True)", default=True)
     a.add_argument("-tstart", type=int, help="Tstart in samples (def:0)", default=0)
     a.add_argument("-tend", type=int, help="Tend in samples (inclusive) (def:1)", default = 1)
+    a.add_argument("-dedisp_pccc", type=int, help="Dedisperse by x DM units (pc/cc)", default = None)
+    a.add_argument("-calib", type=str, help="Path to the calibration soln", default=None)
+    a.add_argument("-sky_subtract", action='store_true', help="Run sky subtraction on the data (def:False)", default=False)
+    a.add_argument("--flag-ants", type=str, help="Flag these ants", default=None)
     a.add_argument("-outname", type=str, help="Name of the output file", default=None)
 
     args = a.parse_args()
