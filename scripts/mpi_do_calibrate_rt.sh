c14215d9588060972540f93aa88b06c1dd00ec2b
Andywang201605 <ztwang201605@gmail.com>
2024-05-09 14:35:07 +0800

    add calibration script for rt

---
+++ scripts/mpi_do_calibrate_rt.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ -z $INDIR ]] ; then
+    echo "No input directory"
+    exit 1
+fi
+indir=$INDIR
+beamno=$OMPI_COMM_WORLD_RANK
+uvfits=$(printf "$indir/b%02d.uvfits" $beamno)
+if [[ ! -f $uvfits ]] ; then
+    echo "UVFITS not found! $uvfits"
+    # don't exit with nonzero oetehrwise the whole mpi job goes down
+    exit 0
+fi
+
+# nsamp=5000
+# outfits=$uvfits.uvw.uvfits
+# rootdir=$(echo $indir | sed s%/data/craco/%/CRACO/DATA_00/%)
+# metafile=$rootdir/../../../SB*.json.gz
+
+# echo "Got fits $uvfits writing to $outfits with metafile $metafile $rootdir"
+
+source /home/craftop/.conda/.remove_conda.sh
+source /home/craftop/.conda/.activate_conda.sh
+conda activate calib
+
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/CRACO/SOFTWARE/craco/wan342/Software/conda3/envs/craco38/lib
+# export PATH=$PATH:/CRACO/SOFTWARE/craco/wan342/bin
+# echo $LD_LIBRARY_PATH
+
+logfile=${uvfits}.cal.log
+
+fixuvfits $uvfits 
+
+# if [[ ! -f $outfits ]] ; then
+#     #attach_uvws_uvfits -outname $outfits -end_samp $nsamp $uvfits $metafile
+#     quicksnip -O $outfits -N $nsamp $uvfits --metadata $metafile $@
+# fi
+
+
+cmd="/CRACO/SOFTWARE/craco/craftop/softwares/craco_calib/calib_skadi.py -uv $uvfits"
+echo running $cmd
+$cmd 
+
+
+
