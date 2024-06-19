e9fe3828d73ba065f79077081e257538297a99de
Keith Bannister <keith.bannister@csiro.au>
2024-06-19 15:22:58 +0800

    Add UCX environment to mpi_run_beam.sh

--- scripts/mpi_run_beam.sh
+++ scripts/mpi_run_beam.sh
@@ -34,7 +34,44 @@ beam_only_rankfile=$indir/beam_only.rank
 beam_rank_file $rankfile > $beam_only_rankfile
 
 rootdir=$(dirname $0)
-cmd="mpirun -wdir $indir -rankfile $beam_only_rankfile -x INDIR=$indir -x START_CARD -x RUNNAME $@ "
+
+# Setup environment for networking
+
+use_roce=1
+enable_hcoll=1
+verbose=0
+oob_verbose=0
+export HCOLL_VERBOSE=$verbose
+if [[ $use_roce == 1 ]] ; then
+    echo "Setting up for RoCE"
+    export UCX_NET_DEVICES=mlx5_0:1,mlx5_2:1
+    export UCX_TLS=self,mm,cma,rc,rc_mlx5,ud,ud_mlx5
+    export UCX_IB_GID_INDEX=0
+else
+    echo "Setting up for TCP"
+    if [[ $(hostname) == "athena" ]] ; then
+        dev=eno1
+    fi
+
+    export UCX_NET_DEVICES=ens3f0np0,ens6f0np0
+    export UCX_TLS=self,tcp,mm,cma
+fi
+ifaces=$UCX_NET_DEVICES
+# use OB1 and TCP
+tcpargs=" --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include $ifaces --mca oob_tcp_if_include $ifaces --mca coll_hcoll_enable $enable_hcoll -x coll_hcoll_np=0 --mca orte_base_help_aggregate 0"
+
+# USE UCX
+ucxargs="--mca pml ucx -x UCX_TLS -x UCX_IB_GID_INDEX -x UCX_NET_DEVICES --mca oob_tcp_if_include eno8303 --mca oob_base_verbose $oob_verbose --mca coll_hcoll_enable $enable_hcoll -x HCOLL_VERBOSE --mca pml_ucx_verbose $verbose"
+
+
+echo UCX_TLS=$UCX_TLS
+echo UCX_IB_GID_INDEX=$UCX_IB_GID_INDEX
+echo UCX_IB_SL=$UCX_IB_SL
+echo UCX_NET_DEVICES=$UCX_NET_DEVICES
+
+
+
+cmd="mpirun $ucxargs -wdir $indir -rankfile $beam_only_rankfile -x INDIR=$indir -x START_CARD -x RUNNAME $@ "
 echo running $cmd with indir=$indir start_card=$START_CARD
 $cmd
 
