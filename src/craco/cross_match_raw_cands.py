fe890afd8edce8f9f80f8061a6a99f2976a775a5
vivgastro <vivg269@gmail.com>
2024-07-26 14:37:59 +0800

    Got the cross_match_cands logic simplified and working with Yuanming

--- src/craco/cross_match_raw_cands.py
+++ src/craco/cross_match_raw_cands.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from candidate_manager import SBCandsManager
-import glob
-
-
+import argparse
+import logging
+import os
 
 def get_uniq_classification(clustered_cand):
     '''
@@ -33,88 +33,109 @@ def get_uniq_classification(clustered_cand):
 
     return label, alias
 
+def get_RFI_classification(clustered_rfi_cand):
+    if (126 <= clustered_rfi_cand['lpix'].values <= 130) and (126 <= clustered_rfi_cand['mpix'].values <= 130):
+        label='CGHOST'
+    elif (clustered_rfi_cand['num_samps'].values < 3) and (clustered_rfi_cand['num_spatial'].values == 1):
+        label='NOISE'
+    else:
+        label='RFI'
+    return label
 
 def main():
-    x = SBCandsManager("SB063225")
 
+    log.info(f"Working on SBID - {args.sbid}")
+    x = SBCandsManager(args.sbid)
+    log.debug(f"Found {x.n_clusteredrawcandfiles} rawcat candfiles to add labels to")
     for candfile in x.clustered_raw_candfiles:
-        print(candfile)
+        log.debug(f"Working on candfile - {candfile}")
         beamid = candfile.beamid
         clustered_rfi_file = x.filter_candfiles(x.clustered_rfi_candfiles, beamid=beamid, tstart=candfile.tstart, scanid=candfile.scanid)
         clustered_uniq_file = x.filter_candfiles(x.clustered_uniq_candfiles, beamid=beamid,tstart=candfile.tstart, scanid=candfile.scanid)
         for ii, icand in candfile.cands.iterrows():
             alias = 0
             cluster_id_in_rfi = (clustered_rfi_file[0].cands['cluster_id'].values == int(icand['cluster_id']))
-            spatial_id_in_rfi = (clustered_rfi_file[0].cands['spatial_id'].values == int(icand['spatial_id']))
-            
-            is_it_in_rfi = np.where( cluster_id_in_rfi & spatial_id_in_rfi  )
-            
             cluster_id_in_uniq = (clustered_uniq_file[0].cands['cluster_id'].values == int(icand['cluster_id']))
-            spatial_id_in_uniq = (clustered_uniq_file[0].cands['spatial_id'].values == int(icand['spatial_id']))
-            
-            is_it_in_uniq = np.where(  cluster_id_in_uniq  & spatial_id_in_uniq )
-            if len(is_it_in_rfi[0]) > 0:
-                #if int(icand['cluster_id']) in clustered_rfi_file[0].cands['cluster_id'].values and int(icand['spatial_id']) in clustered_rfi_file[0].cands['spatial_id'].values:
-                #loc = np.where( (clustered_rfi_file[0].cands['cluster_id'].values == int(icand['cluster_id'])) &\
-                                #(clustered_rfi_file[0].cands['spatial_id'].values == int(icand['spatial_id'])) )[0]
-                loc = is_it_in_rfi[0]
-                clustered_rfi_cand = clustered_rfi_file[0].cands.iloc[loc]
-                #print('crfi', clustered_rfi_cand,"--->", clustered_rfi_cand['num_samps'],"====>", type(clustered_rfi_cand['num_samps']))
-                
-                if (126 <= icand['lpix'] <= 130) and (126 <= icand['mpix'] <= 130):
-                    label='CGHOST'
-                elif clustered_rfi_cand['num_samps'].values[0] < 3:
-                    label='NOISE'
-                else:
-                    label='RFI'
     
-            elif len(is_it_in_uniq[0]) > 0:
-                #elif int(icand['cluster_id']) in clustered_uniq_file[0].cands['cluster_id'].values and int(icand['spatial_id']) in clustered_uniq_file[0].cands['spatial_id'].values:
-                #loc = np.where(  (clustered_uniq_file[0].cands['cluster_id'].values == int(icand['cluster_id']))  &\
-                #                 (clustered_uniq_file[0].cands['spatial_id'].values == int(icand['spatial_id']))  )[0]
-                loc = is_it_in_uniq[0]
-                clustered_uniq_cand = clustered_uniq_file[0].cands.iloc[loc]
-                cuc = clustered_uniq_cand
+            ncands_in_rfi = len(clustered_rfi_file[0].cands[cluster_id_in_rfi])
+            ncands_in_uniq = len(clustered_uniq_file[0].cands[cluster_id_in_uniq])
     
-                label, alias =get_uniq_classification(clustered_uniq_cand)
-                #print('cuc', cuc)
+            if ncands_in_uniq > 0:
     
-            else:
-                is_only_cluster_in_rfi =  np.where(cluster_id_in_rfi)
-                is_only_cluster_in_uniq = np.where(cluster_id_in_uniq)
+                if ncands_in_uniq == 1:
+                    label, alias = get_uniq_classification(clustered_uniq_file[0].cands[cluster_id_in_uniq])
     
-                if len(is_only_cluster_in_rfi[0]) > 2:
-                    #This should never happen - there is a bug in Yuanming's code - blame her
-                    raise Exception("Yuanming messed up")
-                elif len(is_only_cluster_in_rfi[0]) > 0:
-                    #All siblings of this candidate ended up in the RFI bucket, so we can safely regard this raw candidate as RFI too
-    
-                    #There is no way that NOISE AND CENTRAL GHOST can have siblings(separate spatial clusters with same cluster id) that get dropped
-                    #So we assume that they are all RFI
-                    label='RFI'
-                elif len(is_only_cluster_in_uniq[0])>2:
-                    #This should never happen - there is a bug in Yuanming's code - blame her
-                    raise Exception("Yuanming messed up")
-                elif len(is_only_cluster_in_uniq[0]) > 0:
-                    locs = is_only_cluster_in_uniq[0]
-                    if len(locs) > 1:
-                        try:
-                            brightest_sibling_loc = [np.argmax(clustered_uniq_file[0].cands.iloc[locs]['SNR'])]
-                        except KeyError as KE:
-                            brightest_sibling_loc = [np.argmax(clustered_uniq_file[0].cands.iloc[locs]['snr'])]
-                        brightest_sibling = clustered_uniq_file[0].cands.iloc[locs].iloc[brightest_sibling_loc]
-                    else:
-                        brightest_sibling = clustered_uniq_file[0].cands.iloc[locs]
-                        
-                    label, alias = get_uniq_classification(brightest_sibling)
+                elif ncands_in_uniq == 2:
                     
+                    spatial_id_in_uniq = (clustered_uniq_file[0].cands['spatial_id'].values == int(icand['spatial_id'])) & cluster_id_in_uniq
+                    nspatial_match_in_uniq = len(clustered_uniq_file[0].cands[spatial_id_in_uniq])
+    
+                    if nspatial_match_in_uniq == 1:
+                        #We have got a match of cluster id and spatial id in the uniq file - simple case, get the classification
+                        label, alias = get_uniq_classification(clustered_uniq_file[0].cands[spatial_id_in_uniq])
+                    
+                    elif nspatial_match_in_uniq == 0:
+                        # We got a match of the cluster id, but not the spatial id - so now we assume that the spatial id for this icand was dropped
+                        #So we have to pick the brightest candidate that made it, and associate icand with that candidate
+                        try:
+                            brightest_sibling_loc = [np.argmax(clustered_uniq_file[0].cands.iloc[cluster_id_in_uniq]['SNR'])]
+                        except KeyError as KE:
+                            brightest_sibling_loc = [np.argmax(clustered_uniq_file[0].cands.iloc[cluster_id_in_uniq]['snr'])]
+                        brightest_sibling = clustered_uniq_file[0].cands.iloc[cluster_id_in_uniq].iloc[brightest_sibling_loc]
+                        
+                        label, alias = get_uniq_classification(brightest_sibling)
+    
                 else:
-                    raise RuntimeError(f"Could not find cluster id {int(icand['cluster_id'])} and {int(icand['spatial_id'])} in the RFI or uniq files")
+                    log.critical("Something unexpected happened in the unique classification")
+                    raise RuntimeError("Something unexpercted happened in uniq classification")
+    
+            
+            elif ncands_in_rfi > 0:
+                #If we have found the cluster id of this candidate in the RFI file
+                #Then we try to determine which type of RFI was this candidate
+                alias = 0
+                #print("I was here " + str(ncands_in_rfi))
+                #print(clustered_rfi_file[0].cands[cluster_id_in_rfi], type(clustered_rfi_file[0].cands[cluster_id_in_rfi]))
+                if ncands_in_rfi == 1:
+                    label = get_RFI_classification(clustered_rfi_file[0].cands[cluster_id_in_rfi])
+                elif ncands_in_rfi == 2:
+                    spatial_id_in_rfi = (clustered_rfi_file[0].cands['spatial_id'].values == int(icand['spatial_id'])) & cluster_id_in_rfi
+                    nspatial_match_in_rfi = len(clustered_rfi_file[0].cands[spatial_id_in_rfi])
+                    
+                    if nspatial_match_in_rfi == 1:
+                        #print(type(clustered_rfi_file[0].cands[spatial_id_in_rfi]), clustered_rfi_file[0].cands[spatial_id_in_rfi])
+                        label = get_RFI_classification(clustered_rfi_file[0].cands[spatial_id_in_rfi])
+                    elif nspatial_match_in_rfi == 0:
+                        #Neither CGHOST nor NOISE candidates should have sidelobes. So the fact that this candidate is missing must mean
+                        #that it is a sidelobe of something that is not CGHOST OR NOISE, i.e. RFI
+                        label = 'RFI'
+                    else:
+                        log.critical("Something unexpected happened in rfi classification")
+                        raise RuntimeError("Something unexpected happened in rfi classification")
+            else:
+                log.critical("Something very seriously went wrong in the candpipe")
+                raise RuntimeError("Something very seriously went wrong in the candpipe")
     
             candfile.cands.loc[ii, 'LABEL'] = label
             candfile.cands.loc[ii, 'ALIASED'] = alias
-    print("DONE")        
+
+        outname = candfile.fname.strip(".csv") + ".labelled.csv"
+        log.info(f"Saving the output to {outname}")
+        candfile.cands.to_csv(outname, index=False)
+
+    log.info(f"Finished processing - {args.sbid}")
+    log.info("----------------------------------------------------")
 
 
+if __name__ == '__main__':
 
-main()
+    
+    log = logging.getLogger(__name__)
+    logging.basicConfig(filename="/CRACO/SOFTWARE/craco/craftop/logs/cross_match_raw_cands.log",
+                        format='[%(asctime)s] %(levelname)s: %(message)s', 
+                        level=logging.DEBUG)
+
+    a = argparse.ArgumentParser()
+    a.add_argument("sbid", type=str, help="SBID to process")
+    args = a.parse_args()
+    main()
