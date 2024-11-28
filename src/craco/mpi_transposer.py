f9d400275a130faab1ba40277235600b9074d7aa
Keith Bannister <keith.bannister@csiro.au>
2024-11-28 21:20:17 +0800

    Add custom mpi transposer

---
+++ src/craco/mpi_transposer.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+"""
+Template for making scripts to run from the command line
+
+Copyright (C) CSIRO 2022
+"""
+import pylab
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import logging
+import craco.card_averager
+from craco.timer import Timer
+
+
+import mpi4py.rc
+mpi4py.rc.threads = False
+from mpi4py import MPI
+import mpi4py.util.dtlib
+
+
+log = logging.getLogger(__name__)
+
+__author__ = "Keith Bannister <keith.bannister@csiro.au>"
+
+
+class CustomMpiTransposer:
+    def __init__(self, comm, nbeam, nrx, dtype):            
+        self.comm = comm
+        self.rank = comm.Get_rank()
+        self.nbeam = nbeam
+        self.nrx = nrx
+        self.requests = []
+        self.dtype = dtype
+        self.mpi_dtype = mpi4py.util.dtlib.from_numpy_dtype(self.dtype)
+
+
+    def rank_of_beam(self, ibeam):
+        '''
+        Ranks are ordered 0->nrx-1 | nrx -> nrx + nbeam-1
+        '''
+        return self.nrx + ibeam
+
+    def rank_of_rx(self, irx):
+        return irx
+
+    def _post(self, req):
+        self.requests.append(req)
+
+    def wait(self):
+        for r in self.requests:
+            r.wait()
+        
+        self.requests.clear()
+
+
+
+class MpiTransposeSender(CustomMpiTransposer):
+    def send(self, dtx, do_async=False):
+        '''
+        do-async=True tends to add a lot more jitter
+        '''
+        assert len(dtx) == self.nbeam
+        t = Timer()
+        for ibeam in range(self.nbeam):
+            # make sure everyone doesn't send to the same place
+            # at once
+            destbeam = (ibeam + self.rank) % self.nbeam
+            destrank = self.rank_of_beam(destbeam)
+            #print(f'r{self.rank} sending to r{destrank} beam={destbeam}')
+            if do_async:
+                self._post(self.comm.Isend([dtx[destbeam:destbeam+1], self.mpi_dtype], dest=destrank))
+                t.tick('Isend')
+            else:
+                self.comm.Send([dtx[destbeam:destbeam+1], self.mpi_dtype], dest=destrank)
+                t.tick('send', args={'destbeam':destbeam, 'destrank':destrank})
+            
+            #print(f'r{self.rank} sent to r{destrank} beam={destbeam}')
+
+        if do_async:
+            self.wait()
+            t.tick('Wait')
+        
+
+
+class MpiTransposeReceiver(CustomMpiTransposer):
+
+    def Irecv(self, drx):
+        assert len(drx) == self.nrx
+        my_beamid = self.rank - self.nrx
+        assert my_beamid >= 0
+        t = Timer()
+        for irx in range(self.nrx):
+            sourcerx = (my_beamid - irx) % self.nrx
+            sourcerank = self.rank_of_rx(sourcerx)
+            #print(f'r{self.rank} waiting for r{sourcerank} rx={sourcerx}')
+            #print(drx[irx].dtype, drx[irx].shape, self.dtype, self.mpi_dtype)
+            # Need to give it a length=1 array, otherwise it complains it's got 
+            # a scalar and .... complains.
+            self._post(self.comm.Irecv([drx[irx:irx+1], self.mpi_dtype], source=sourcerank))
+            t.tick('Irecv', args={'sourcerx':sourcerx, 'sourcerank':sourcerank})
+            #print(f'r{self.rank} recieved for r{sourcerank} rx={sourcerx}')
+
+        return self.requests
+
+
+    def recv(self, drx):
+        self.Irecv(drx)
+        self.wait()
+
+
+
+
+def _main():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description='Script description', formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+    parser.set_defaults(verbose=False)
+    values = parser.parse_args()
+    if values.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+    nbeam = 36
+    nant = 24
+    nc = 24
+    nt = 32
+    npol = 1
+    vis_fscrunch = 6
+    vis_tscrunch = 1
+    real_dtype = np.float32
+    cplx_dtype = np.float32
+    nrx = 72
+    dt = craco.card_averager.get_averaged_dtype(nbeam, nant, nc, nt, npol, vis_fscrunch, vis_tscrunch, real_dtype, cplx_dtype)
+    comm = MPI.COMM_WORLD
+    nprocs = nrx + nbeam
+    nranks = comm.Get_size()
+    assert nranks  == nprocs, f'Expected {nprocs} but got size {nranks}'
+    rank = comm.Get_rank()
+
+    niter = 100
+    t = Timer()
+
+    if rank < nrx:
+        sender = MpiTransposeSender(comm, nbeam, nrx, dt)
+        if rank == 0:
+            print(sender.dtype, sender.mpi_dtype)
+        dtx = np.zeros((nbeam), dtype=dt)
+        sender.send(dtx)
+
+        t_start = MPI.Wtime()
+        for i in range(niter):
+            sender.send(dtx)
+            t.tick('send')
+        t_end = MPI.Wtime()
+    else:
+        receiver = MpiTransposeReceiver(comm, nbeam, nrx, dt)
+        drx = np.zeros((nrx), dtype=dt)        
+        receiver.recv(drx)
+
+
+        t_start = MPI.Wtime()       
+        for i in range(niter):
+            receiver.Irecv(drx)
+            receiver.wait()
+            t.tick('recv')
+        t_end = MPI.Wtime()
+        
+    latency = (t_end - t_start)/niter
+
+    print(f'r{rank} latency', latency)
+    
+
+if __name__ == '__main__':
+    _main()
