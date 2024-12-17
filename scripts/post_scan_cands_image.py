ad420d6d7348bdf085e7a84796daaa2c15dc67f9
Andywang201605 <ztwang201605@gmail.com>
2024-12-17 17:59:30 +0800

    integrate posting to the pipeline

--- scripts/post_scan_cands_image.py
+++ scripts/post_scan_cands_image.py
@@ -4,9 +4,26 @@ from craco.craco_run.slackpost import RealTimeScanAlarm
 CAND_POST_TS_SOCKET = "/data/craco/craco/tmpdir/queues/cands"
 TMPDIR = "/data/craco/craco/tmpdir"
 
+def format_outdir(outdir):
+    parts = outdir.strip().split("/")
+    if len(parts) > 0:
+        for ip, part in enumerate(parts):
+            if part.startswith("SB0"):
+                sbid = part
+                scanid = parts[ip + 2]
+                tstart = parts[ip + 3]
+                
+                if len(sbid) == 8 and len(scanid) == 2 and len(tstart) == 14:
+                    return f"{sbid}/scans/{scanid}/{tstart}"
+
+    raise RuntimeError(f"Could not parse sbid, scanid and tstart from {outdir}")
+
 def run_cand_post(outdir, test=False):
     if test: channel = "C06C6D3V03S"
     else: channel = "C085V0B3D7A"
+
+    ### we need the ourdir to be SB0XXXX/scans/00/20240101100000
+    outdir = format_outdir(outdir)
 
     scanalarm = RealTimeScanAlarm(outdir, channel=channel, postlimit=10)
     scanalarm.run_main()
